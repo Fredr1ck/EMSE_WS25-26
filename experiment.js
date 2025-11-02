94d2f90f9561c71c34ccc06318fa85f362833ef6
Fredr1ck <Fredrick.johny01@gmail.com>
2025-11-02 02:34:50 +0100

    Updated experiment.js

--- experiment.js
+++ experiment.js
@@ -1,6 +1,37 @@
 let SEED = "666";
 Nof1.SET_SEED(SEED);
 
+
+
+
+const SHORT_NAMES = ["cid","uid","cfg","usrNm","pos","tmp","val","idx",
+                            "ref","cnt","err", "msg","buf","str","dat",
+                            "arg", "len","num","arr","src"];
+
+
+
+
+const LONG_NAMES = [  "CharacterID","UserID","ConfigurationFile","UserName","Position","TemporaryValue","Value","Index",
+                              "ReferenceCount","Counter","ErrorMessage","ResponseMessage","ResultBuffer","StringData","DataObject",
+                              "ArgumentList", "LengthInBytes","NumericValue","ArrayElements","SourcePath"];
+
+
+const UNIVERSAL_LOOPS = [
+    "for (let {VAR} of users) { /* ... */ }",
+    "if ({VAR} > 0) { /* ... */ }",
+    "while ({VAR} < limit) { /* ... */ }",
+    "console.log({VAR});",
+    "return {VAR};",
+    "let total = {VAR} + 1;",
+    "if ({VAR} === true) { /* ... */ }",
+    "const result = process({VAR});",
+    "for (const key in {VAR}) { /* ... */ }",
+    "update({VAR});"
+];
+
+
+
+
 let experiment_configuration_function = (writer) => { return {
 
     experiment_name: "TestExperiment",
@@ -8,7 +39,7 @@ let experiment_configuration_function = (writer) => { return {
 
     introduction_pages: writer.stage_string_pages_commands([
         writer.convert_string_to_html_string(
-            "Please, open the browser in fullscreen mode (probably by pressing [F11]).\\ If Hello world is shown, press [1], otherwise [0]"),
+            "Please, open the browser in fullscreen mode (probably by pressing [F11]).\\ For each variable name shown: Press [1] if it seems understandable, or [0] if it does not."),
     ]),
 
     pre_run_training_instructions: writer.string_page_command(
@@ -35,11 +66,11 @@ let experiment_configuration_function = (writer) => { return {
                  die unterschiedlichen Werte stehen als List in den Treatments
                  Im ersten Experiment hat man normalerweise nur eine Variable mit 2 Treatments (Werte für die Variable)
          */
-        { variable: "MyVariable",  treatments: ["dummy", "non-dummy"]},
+        { variable: "NameLength",  treatments: ["short", "long"]},
     ],
 
     /* ToDo: Hier gebe ich an, wie oft ich jede Treatmentkombination im Experiment testen möchte */
-    repetitions: 100,
+    repetitions: 3,
 
     /* ToDo: Hier gebe ich an, welche "Art" das Experiment ist. Ich gehe hier davon aus, dass es ein Experiment ist,dass
     *        darauf wartet, dass der Teilnehmer die Taste "0" oder "1" drückt
@@ -50,36 +81,58 @@ let experiment_configuration_function = (writer) => { return {
 
         task.do_print_task = () => {
 
-            // So erzeuge ich eine Zufallszahl (NICHT "default"-Code a la StackOverflow verwenden!
-            let random_int_from_0_to_excluding_10 = Nof1.new_random_integer(10);
-
-            // Ausgabebildschirm wird gelöscht
+            // Altes Zeug löschen
             writer.clear_stage();
 
-            // Guck, weist den Wert der ersten Experiment-Variablen (task.treatment_combination.treatment_combination[0])
-            // der lokalen Variablen treatment_of_variable_MyVariable zu.
-            let treatment_of_variable_MyVariable = task.treatment_combination.treatment_combination[0].value;
 
-            // Testet, ob Wert von MyVariable den Wert "dummy" hat
-            if( treatment_of_variable_MyVariable =="dummy") {
-                writer.print_html_on_stage("Hello, world + random number: " + random_int_from_0_to_excluding_10);
-                writer.print_html_on_stage("<h1>Mal reinen HTMLcode reingeschrieben</h1>");
-                task.expected_answer = "1";
+            // Holt den aktuellen Wert aus dem Experiment (short oder long)
+            let treatment_of_variable_NameLength = task.treatment_combination.treatment_combination[0].value;
+
+            // random index generieren
+            let random_index = Nof1.new_random_integer(SHORT_NAMES.length);
+
+            // Wenn "short" nimm aus der kurzen Liste else aus der langen
+            let variableName = (treatment_of_variable_NameLength == "short") ? SHORT_NAMES[random_index] : LONG_NAMES[random_index];
+
+
+            // wähle zufällig, ob nur Name oder Name + Code gezeigt wird
+            let showAsSnippet = Nof1.new_random_integer(2) === 1; // 50/5ß chance
+
+
+            // wenn Snippet, zufälliges Template auswählen
+            let displayText = ""; // Variable für den späteren Text
+            if (showAsSnippet) {
+
+                // Wählt zufällig eine Vorlage aus der UNIVERSAL_LOOPS-Liste
+                let random_loop = UNIVERSAL_LOOPS[Nof1.new_random_integer(UNIVERSAL_LOOPS.length)];
+
+                // Ersetzt in der Vorlage den Platzhalter {VAR} durch Variablennamen
+                let loop_with_var = random_loop.replaceAll("{VAR}", variableName);
+
+                // Baut den Text für die Anzeige zusammen
+                displayText = "Code snippet:<br><pre>" + loop_with_var + "</pre>";
             } else {
-                writer.print_html_on_stage("Exit world + random number: " + random_int_from_0_to_excluding_10);
-                task.expected_answer = "0";
+
+                // Wenn kein Code aus der Vorlage dann nur den Variablennamen
+                displayText = "Variable name: <b>" + variableName + "</b>";
             }
+
+
+            writer.print_html_on_stage(
+                displayText + "<br><br>Press [1] if understandable, [0] if not understandable."
+            );
+
+            // keine bestimmte "richtige" Taste erwartet (weil subjektive Einschätzung)
+            task.expected_answer = null;
         };
 
         /* ToDo: Legt fest, wann eine Aufgabe als bearbeitet angesehen wird. Die Variable "answer" ist dabei die Taste, die gedrückt wurde.
                  Falls es für das Experiment egal ist, einfach true zurückgeben.
         *  */
         task.accepts_answer_function = (answer) => {
-            if (answer == task.expected_answer) {
-                return true;
-            } else {
-                return false;
-            }
+
+            return true
+
         }
 
         /**
@@ -96,7 +149,7 @@ let experiment_configuration_function = (writer) => { return {
         task.do_print_after_task_information = () => {
             writer.clear_stage();
             writer.print_html_on_stage(
-                writer.convert_string_to_html_string("Ok, good answer. When you press [Enter] the experiment goes on."));
+                writer.convert_string_to_html_string("When you press [Enter] the experiment goes on."));
         }
     }
 }};
